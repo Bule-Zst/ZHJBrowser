40520f767fc654ab4e8eada5079e42f127c7c418
Bule-Zst <1583245158@qq.com>
2018-10-30 11:02:02 +0800

    some global functions and variables

--- app/src/main/java/com/zhj/browser/App.kt
+++ app/src/main/java/com/zhj/browser/App.kt
@@ -1,6 +1,7 @@
 package com.zhj.browser
 
 import android.app.Application
+import android.util.Log
 import java.time.Instant
 
 //全局的context，方便代码编写
@@ -13,4 +14,12 @@ class App: Application() {
         super.onCreate()
         instant = this
     }
+}
+
+fun log( msg: String ) {
+    Log.i( "Bule", msg )
+}
+
+fun debug( msg: String ) {
+    Log.d( "Bule", msg )
 }
---
+++ app/src/main/java/com/zhj/browser/tool/BitmapTool.kt
@@ -0,0 +1,51 @@
+package com.zhj.browser.tool
+
+import android.graphics.Bitmap
+import com.zhj.browser.App
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
+
+object BitmapTool {
+    private val IN_PATH = "/icon/"
+
+    /**
+     * 随机生产文件名
+     *
+     * @return
+     */
+    private fun generateFileName(): String {
+        return UUID.randomUUID().toString()
+    }
+
+    /**
+     * 保存bitmap到本地
+     *
+     * @param context
+     * @param mBitmap
+     * @return
+     */
+    fun saveBitmap(mBitmap: Bitmap): String? {
+        var savePath: String = App.instant.filesDir.absolutePath + IN_PATH
+        var filePic: File
+        try {
+            filePic = File(savePath + generateFileName() + ".jpg")
+            if (!filePic.exists()) {
+                filePic.getParentFile().mkdirs()
+                filePic.createNewFile()
+            }
+            val fos = FileOutputStream(filePic)
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            fos.flush()
+            fos.close()
+        } catch (e: IOException) {
+            // TODO Auto-generated catch block
+            e.printStackTrace()
+            return null
+        }
+
+        return filePic.absolutePath
+    }
+}
