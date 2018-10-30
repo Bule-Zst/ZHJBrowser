0a09237bfad758983baf4908138e75bb9b1d4d73
Bule-Zst <1583245158@qq.com>
2018-10-30 11:03:41 +0800

    change for extend activity

--- app/src/main/java/com/zhj/browser/extend/ExtendActivity.kt
+++ app/src/main/java/com/zhj/browser/extend/ExtendActivity.kt
@@ -3,6 +3,11 @@ package com.zhj.browser.extend
 import android.app.Activity
 import android.os.Bundle
 import com.zhj.browser.R
+import com.zhj.browser.database.AppDatabase
+import com.zhj.browser.database.Item
+import com.zhj.browser.database.toMsg
+import com.zhj.browser.database.toast
+import com.zhj.browser.debug
 
 //收藏夹、历史记录、本地保存都共用一个Activity
 class ExtendActivity : Activity() {
@@ -11,7 +16,7 @@ class ExtendActivity : Activity() {
         setContentView( R.layout.extend )
 
 //        根据MainActivity的传参决定渲染哪个功能
-        val choose = this.intent.getIntExtra( "choose", 0 )
+        val choose = this.intent.getIntExtra( "choose", Item.HISTORY )
         when( choose ) {
             0 -> {
                 drawFavour()
@@ -28,7 +33,12 @@ class ExtendActivity : Activity() {
 //    获取历史记录的数据，并渲染在Activity中
 //    以下两个draw函数，功能类似
     fun drawHistory() {
-
+        val db = AppDatabase.getInstance()
+        val list = db.getDao().queryByCategory( Item.HISTORY )
+        db.close()
+//        开始渲染
+        debug( list.toMsg() )
+        toast( list.toMsg() )
     }
     fun drawFavour() {
 
